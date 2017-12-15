e697bf076f0c8ab19deb9014ffc2b917f7499b66
paramedios <gonsebmedina@gmail.com>
2017-12-15 01:56:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,32 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var streamTitle = require('stream-title');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+							 
+							 
+  socket.on('estado streaming', function(msg){
+  console.log('listening on *:' + msg);
+	  
+	  
+  streamTitle({
+    url: 'http://s2.netradiofm.com:'+msg,
+    type: 'shoutcast2',
+    sid: 1
+}).then(function (title) {
+    console.log(title);
+    io.emit('estado streaming', title);
+}).catch(function (err) {
+    console.log(err);
+});
+
+
+});
 });
 
 http.listen(port, function(){
