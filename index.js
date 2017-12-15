483eb9f19e0a15256923a6f732a2d4ca6bb41824
paramedios <gonsebmedina@gmail.com>
2017-12-15 04:57:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var streamTitle = require('stream-title');
-const u = require('url');
-const rp = require('request-promise');
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -13,7 +12,7 @@ app.get('/', function(req, res){
 
 
 
-function ShoutcastV2(url, sid) {
+function getByShoutcast2(url, sid) {
 
     if(typeof url === 'undefined')
         throw new Error('required url');
@@ -29,6 +28,30 @@ function ShoutcastV2(url, sid) {
     });
 
 }
+
+
+
+function verStreaming(args) {
+
+    if(typeof args.type === 'undefined')
+        throw new Error('required type');
+
+    switch (args.type) {
+        case 'shoutcast2':
+            return getByShoutcast2(args.url, args.sid);
+            break;
+
+        case 'icecast':
+            return getByIcecast(args.url, args.mount);
+            break;
+
+        case 'shoutcast':
+            return getByShoutcast(args.url);
+            break;
+    }
+
+}
+
 
 
 
@@ -55,9 +78,9 @@ io.on('connection', function(socket){
 	
 
 	  
-  VerStreaming({
+  verStreaming({
     url: 'http://s2.netradiofm.com:'+msg,
-    type: 'ShoutcastV2',
+    type: 'shoutcast2',
     sid: 1
 }).then(function (data) {
     console.log(data);
@@ -73,29 +96,5 @@ io.on('connection', function(socket){
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
-
-
-
-function VerStreaming(args) {
-
-    if(typeof args.type === 'undefined')
-        throw new Error('required type');
-
-    switch (args.type) {
-        case 'shoutcast2':
-            return ShoutcastV2(args.url, args.sid);
-            break;
-
-        case 'icecast':
-            return getByIcecast(args.url, args.mount);
-            break;
-
-        case 'shoutcast':
-            return getByShoutcast(args.url);
-            break;
-    }
-
-}
-
 
 
