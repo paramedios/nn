b134bfe0da79d1eb93051542045c82b6027c6fdf
paramedios <gonsebmedina@gmail.com>
2017-12-15 03:33:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,58 +2,11 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-var u = require('url');
-var rp = require('request-promise');
+var streamTitle = require('stream-title');
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
-
-function ShoutcastV2(url, sid) {
-
-    if(typeof url === 'undefined')
-        throw new Error('required url');
-
-    if(typeof sid === 'undefined')
-        throw new Error('required sid');
-
-    url = url + '/stats?sid=' + sid + '&json=1';
-
-    return rp(url).then(function (body) {
-        let data = JSON.parse(body);
-        return data
-    });
-
-}
-
-
-
-
-
-
-function VerStreaming(args) {
-
-    if(typeof args.type === 'undefined')
-        throw new Error('required type');
-
-    switch (args.type) {
-        case 'shoutcast2':
-            return ShoutcastV2(args.url, args.sid);
-            break;
-
-        case 'icecast':
-            return getByIcecast(args.url, args.mount);
-            break;
-
-        case 'shoutcast':
-            return getByShoutcast(args.url);
-            break;
-    }
-
-}
-
-
-
 
 
 
@@ -70,19 +23,19 @@ io.on('connection', function(socket){
 		
 		
 							 
-  socket.on('estado streaming', function(streaming){
-  console.log('listening on *:' + streaming);
+  socket.on('estado streaming', function(msg){
+  console.log('listening on *:' + msg);
 	
 	
 	
 	  
-  VerStreaming({
-    url: 'http://s2.netradiofm.com:'+streaming,
+  streamTitle({
+    url: 'http://s2.netradiofm.com:'+msg,
     type: 'shoutcast2',
     sid: 1
 }).then(function (data) {
-    console.log(data['songtitle']);
-    io.emit('estado streaming', '{"id":"'+data['streamhits']+'","sonando":"'+data['songtitle']+'","estado":"'+data['streamstatus']+'","puerto":"'+streaming+'"}');
+    console.log(data);
+    io.emit('estado streaming', '{"tema":true,"portada":"'+data+'","programa":"gggg"}');
 }).catch(function (err) {
     console.log(err);
 });
