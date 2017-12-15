3d1615fef506f593b682f909b63acfea2602f690
paramedios <gonsebmedina@gmail.com>
2017-12-15 05:03:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var streamTitle = require('stream-title');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
