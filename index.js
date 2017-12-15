6fb469ae707443e21295f88e70a1b06584fa5ff9
paramedios <gonsebmedina@gmail.com>
2017-12-15 03:20:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,87 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-var streamTitle = require('stream-title');
-
+var u = require('url');
+var rp = require('request-promise');
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+
+function ShoutcastV2(url, sid) {
+
+    if(typeof url === 'undefined')
+        throw new Error('required url');
+
+    if(typeof sid === 'undefined')
+        throw new Error('required sid');
+
+    url = url + '/stats?sid=' + sid + '&json=1';
+
+    return rp(url).then(function (body) {
+        let data = JSON.parse(body);
+        return data
+    });
+
+}
+
+
+
+
+
+
+function VerStreaming(args) {
+
+    if(typeof args.type === 'undefined')
+        throw new Error('required type');
+
+    switch (args.type) {
+        case 'shoutcast2':
+            return ShoutcastV2(args.url, args.sid);
+            break;
+
+        case 'icecast':
+            return getByIcecast(args.url, args.mount);
+            break;
+
+        case 'shoutcast':
+            return getByShoutcast(args.url);
+            break;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
 io.on('connection', function(socket){
+		
+		
+		
+		
+		
+		
 							 
-							 
-  socket.on('estado streaming', function(msg){
-  console.log('listening on *:' + msg);
+  socket.on('estado streaming', function(streaming){
+  console.log('listening on *:' + streaming);
+	
+	
+	
 	  
-	  
-  streamTitle({
-    url: 'http://s2.netradiofm.com:'+msg,
+  VerStreaming({
+    url: 'http://s2.netradiofm.com:'+streaming,
     type: 'shoutcast2',
     sid: 1
-}).then(function (title) {
-    console.log(title);
-    io.emit('estado streaming', title);
+}).then(function (data) {
+    console.log(data['songtitle']);
+    io.emit('estado streaming', '{"id":"'+data['streamhits']+'","sonando":"'+data['songtitle']+'","estado":"'+data['streamstatus']+'","puerto":"'+streaming+'"}');
 }).catch(function (err) {
     console.log(err);
 });
