0fc487240d82bab65e117bcaaeb8429e99f19754
paramedios <gonsebmedina@gmail.com>
2017-12-15 04:59:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,12 @@
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-var streamTitle = require('stream-title');
-
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
-
-
-
-function getByShoutcast2(url, sid) {
-
-    if(typeof url === 'undefined')
-        throw new Error('required url');
-
-    if(typeof sid === 'undefined')
-        throw new Error('required sid');
-
-    url = url + '/stats?sid=' + sid + '&json=1';
-
-    return rp(url).then(function (body) {
-        let data = JSON.parse(body);
-        return data
-    });
-
-}
-
-
-
-function verStreaming(args) {
-
-    if(typeof args.type === 'undefined')
-        throw new Error('required type');
-
-    switch (args.type) {
-        case 'shoutcast2':
-            return getByShoutcast2(args.url, args.sid);
-            break;
-
-        case 'icecast':
-            return getByIcecast(args.url, args.mount);
-            break;
-
-        case 'shoutcast':
-            return getByShoutcast(args.url);
-            break;
-    }
-
-}
-
-
-
-
-
 
 
 
@@ -68,23 +19,26 @@ function verStreaming(args) {
 
 
 io.on('connection', function(socket){
-	
 		
-	
+		
+		
+		
+		
 		
 							 
   socket.on('estado streaming', function(msg){
   console.log('listening on *:' + msg);
 	
-
+	
+	
 	  
-  verStreaming({
+  streamTitle({
     url: 'http://s2.netradiofm.com:'+msg,
     type: 'shoutcast2',
     sid: 1
 }).then(function (data) {
     console.log(data);
-    io.emit('estado streaming', '{"tema":true,"portada":"'+data+'","programa":"'+data['streamstatus']+'"}');
+    io.emit('estado streaming', '{"tema":true,"portada":"'+data+'","programa":"gggg"}');
 }).catch(function (err) {
     console.log(err);
 });
@@ -96,5 +50,3 @@ io.on('connection', function(socket){
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
-
-
